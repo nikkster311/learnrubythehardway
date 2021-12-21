63666aa267ac03cb6567b8f7dd24f41a132f5d40
unknown <nikkster311@gmail.com>
2021-12-20 19:58:35 -0800

    lrthw - ex1

---
+++ exl.rb
@@ -0,0 +1,9 @@
+puts "hello world!"
+puts "hello again"
+puts "I like typing this."
+puts "this is fun"
+puts "yay! printing"
+puts "lets test"
+puts 2+2
+puts "I'd much ranther you 'not'."
+puts 'I "said" do not touch this.'
