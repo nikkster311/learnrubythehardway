c074b57c244cfb5fd18a6b223999af2daac5bb0f
unknown <nikkster311@gmail.com>
2021-12-20 20:04:17 -0800

    lrthw - ex2

--- ex1.rb
+++ ex1.rb
@@ -4,6 +4,6 @@ puts "I like typing this."
 puts "this is fun"
 puts "yay! printing"
 puts "lets test"
-puts 2+2
+puts 2+2 #I was testing this and it worked!
 puts "I'd much ranther you 'not'."
 puts 'I "said" do not touch this.'
