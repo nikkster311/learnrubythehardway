66dedcd05314ef7e2bf4525aabf99a6cd046ccc5
unknown <nikkster311@gmail.com>
2021-12-21 18:33:58 -0800

    lrthw ex11

---
+++ ex11.rb
@@ -0,0 +1,13 @@
+print "How old are you?"
+age = gets.chomp
+print "How tall are you? "
+height = gets
+puts "How much do you weight??   "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall, and #{weight} heavy."
+print "Does this work? #{age + height + weight}"
+
+# looks like print and puts are the same.....
+
+# print allows the users answer to go on teh same line as the string!!
