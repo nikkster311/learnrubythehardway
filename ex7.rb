9924fc60d511c78f285470738cd256b4d63fd4e6
unknown <nikkster311@gmail.com>
2021-12-21 17:37:19 -0800

    lrthw - ex7

---
+++ ex7.rb
@@ -0,0 +1,24 @@
+puts "Mary had a little lamb."
+# below prints snow bc its in quotes and not a var
+puts "Its fleece was white as #{'snow'}."
+puts "And everywhere that Mary went."
+# this is string * 10
+puts "." * 10 # this prints 10 periods!
+
+# can use single or double quotes
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = 'e'
+end5 = 's'
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# looks like puts and print are the same?
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
