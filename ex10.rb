3e2e56ce9e8e006a358af3d6c3cb6fb10e6a14d6
unknown <nikkster311@gmail.com>
2021-12-21 18:21:37 -0800

    lrthw ex10

---
+++ ex10.rb
@@ -0,0 +1,24 @@
+# \t tabs?
+tabby_cat = "\tI'm tabbed in."
+# new line
+persian_cat = "I'm split\non a line."
+# not sure..
+# oh this allows a backslash to show up
+backslash_cat = "I'm \\ a \\ cat."
+
+# tabbed list, each on a new line bc """""" ?
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
+puts "\a"
+puts "\b"
+puts "\f"
