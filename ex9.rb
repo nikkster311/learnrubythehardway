297191b209df7bd074450b5248fd9d244eee3270
unknown <nikkster311@gmail.com>
2021-12-21 18:12:50 -0800

    lrthw ex9

---
+++ ex9.rb
@@ -0,0 +1,20 @@
+# Here's some new strange stuff, remember type it exactly
+
+# normal string
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# this string has line breaks
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# prints days
+puts "Here are the days: #{days}"
+# prints months on new lines
+puts "Here are the months: #{months}"
+
+# not sure about this one.... maybe the q gives us qotes
+# ohh its a formatted, indented quote.
+puts %q{
+    There;s something going on here,
+    With this weird quotes
+    We'll be able to type as much as ew like.
+    Even 4 lines if we want, or 5, or 6.
+}
