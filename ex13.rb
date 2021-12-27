64436ca67f83438c0ab6f4fc3d138e0ebc107869
unknown <nikkster311@gmail.com>
2021-12-27 11:20:34 -0800

    lrthw - ex13

---
+++ ex13.rb
@@ -0,0 +1,9 @@
+first, second, third = ARGV
+
+puts "your first variable is: #{first}"
+puts "your second variable is #{second}"
+puts "your third is #{third}"
+
+puts "here is gets chomp "
+num = $stdin.gets.chomp
+puts "#{num}"
